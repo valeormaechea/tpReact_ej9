5f95fddd7a371d5eb2df9520cab90146082ab821
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-08-28 19:02:37 -0300

    agrego logica al formulario con usestate y effect para crear arreglo de objetos cita

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,46 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Form, Button, Row, Col, Card } from "react-bootstrap";
 import ListaCitas from "./ListaCitas";
 
 const Formulario = () => {
+  let citasLocalStorage =
+    JSON.parse(localStorage.getItem("citasVeterinaria")) || [];
+
+  const [nombreMascota, setNombreMascota] = useState("");
+  const [nombreDuenio, setNombreDuenio] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [hora, setHora] = useState("");
+  const [sintomas, setSintomas] = useState("");
+  const [arregloCitas, setArregloCitas] = useState(citasLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem("citasVeterinaria", JSON.stringify(arregloCitas));
+  }, [arregloCitas]);
+
+  const limpiarFormulario = () => {
+    setNombreMascota("");
+    setFecha("");
+    setNombreDuenio("");
+    setSintomas("");
+    setHora("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hola");
+    const nuevaCita = {
+      nombreMascota,
+      nombreDuenio,
+      fecha,
+      hora,
+      sintomas,
+    };
+    console.log(nuevaCita);
+    // Actualizar el arreglo de colores
+    setArregloCitas([...arregloCitas, nuevaCita]);
+    limpiarFormulario();
+  };
+
   return (
     <article>
       <Card className="mt-5">
@@ -10,28 +48,55 @@ const Formulario = () => {
           <h3 className="my-0">Llene el formulario para crear una cita:</h3>
         </Card.Header>
         <Card.Body>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3 column" controlId="formNombreMascota">
               <Form.Label>Nombre de mascota: </Form.Label>
-              <Form.Control type="text" placeholder="Coco" />
+              <Form.Control
+                type="text"
+                placeholder="Coco"
+                required
+                onChange={(e) => setNombreMascota(e.target.value)}
+                value={nombreMascota}
+              />
             </Form.Group>
             <Form.Group className="mb-3 column" controlId="formNombreDuenio">
               <Form.Label>Nombre del dueño: </Form.Label>
-              <Form.Control type="text" placeholder="Juan Perez" />
+              <Form.Control
+                type="text"
+                placeholder="Juan Perez"
+                required
+                onChange={(e) => setNombreDuenio(e.target.value)}
+                value={nombreDuenio}
+              />
             </Form.Group>
             <Row>
               <Form.Group as={Col} className="mb-3" controlId="formFecha">
                 <Form.Label>Fecha: </Form.Label>
-                <Form.Control type="date" />
+                <Form.Control
+                  type="date"
+                  required
+                  onChange={(e) => setFecha(e.target.value)}
+                  value={fecha}
+                />
               </Form.Group>
               <Form.Group as={Col} className="mb-3" controlId="formHora">
                 <Form.Label>Hora: </Form.Label>
-                <Form.Control type="time" />
+                <Form.Control
+                  type="time"
+                  required
+                  onChange={(e) => setHora(e.target.value)}
+                  value={hora}
+                />
               </Form.Group>
             </Row>
             <Form.Group className="mb-3" controlId="formSintomas">
               <Form.Label>Sintomas: </Form.Label>
-              <Form.Control type="text" />
+              <Form.Control
+                type="text"
+                required
+                onChange={(e) => setSintomas(e.target.value)}
+                value={sintomas}
+              />
             </Form.Group>
             <Button variant="info" type="submit">
               Agregar nueva cita
